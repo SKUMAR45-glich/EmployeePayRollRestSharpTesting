adec61d21b19a91577e657db06edeb2375652a91
Saurabh <saurabh.nit20@gmail.com>
2020-11-17 18:38:08 +0530

    [Saurabh].Deletion of Data in Employee List

--- Employee.cs
+++ Employee.cs
@@ -4,9 +4,10 @@ using System.Text;
 
 namespace RestSharpTesting
 {
+    //Class for Employees 
     public class Employee
     {
-        public int id { get; set; }
+        public int id { get; set; }                                   //Auto GET-SET Properties 
         public string name { get; set; }
         public int salary { get; set; }
     }
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -12,35 +12,37 @@ namespace RestSharpTesting
     [TestClass]
     public class UnitTest1
     {
-        RestClient client;
+        RestClient client;                                                      //Declaration of Client
 
         [TestInitialize]
         public void SetUp()
         {
-            client = new RestClient("http://localhost:4000");
+            client = new RestClient("http://localhost:4000");                      //Setting Link for the Client 
         }
 
         private IRestResponse getEmployeeList()
         {
             //arrange
-            RestRequest request = new RestRequest("/employees", Method.GET);
+            RestRequest request = new RestRequest("/employees", Method.GET);              //Retrieving EmployeesList 
 
             //act
             IRestResponse response = client.Execute(request);
             return response;
         }
-        /*
+        
 
         [TestMethod]
-        public void OnCallingReturnEmployeeList()
+        public void OnCallingReturnEmployeeList()                                               
         {
             IRestResponse response = getEmployeeList();
 
             //Assert
-            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
-            List<Employee> dataResponse = JsonConvert.DeserializeObject<List<Employee>>(response.Content);
-            Assert.AreEqual(10, dataResponse.Count);
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);                                 //Checking the Status
+            List<Employee> dataResponse = JsonConvert.DeserializeObject<List<Employee>>(response.Content);          //Desrializing Object
+            Assert.AreEqual(10, dataResponse.Count);                                                          //Counting the responses that matches
 
+
+            //Retrieving the data from the DataResponse 
             foreach(var item in dataResponse)
             {
                 Console.WriteLine("ID: " + item.id + " Name: " + item.name + " Salary: " + item.salary);
@@ -51,57 +53,70 @@ namespace RestSharpTesting
         [TestMethod]
         public void OnAddingNewDatatoEmployeeList()
         {
-            RestRequest request = new RestRequest("/employees",Method.POST);
+            //Arrange
+            RestRequest request = new RestRequest("/employees",Method.POST);                          //For Adding Data to EmployeeList
             JObject jObject = new JObject();
-            jObject.Add("name", "VK");
+           
+            //Act
+
+            jObject.Add("name", "VK");                                                                  //Addition of Data
             jObject.Add("salary", 80000);
 
-            request.AddParameter("application/json", jObject, ParameterType.RequestBody);
+            request.AddParameter("application/json", jObject, ParameterType.RequestBody);                 //Validation of RestRequest
 
 
-            IRestResponse response = client.Execute(request);
-            Assert.AreEqual(response.StatusCode, HttpStatusCode.Created);
-            Employee dataResponse = JsonConvert.DeserializeObject<Employee>(response.Content);
+            IRestResponse response = client.Execute(request);                                            //Executing the response
 
-            Assert.AreEqual("VK", dataResponse.name);
+
+            //Assert
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.Created);                                //Checking Validation
+            Employee dataResponse = JsonConvert.DeserializeObject<Employee>(response.Content);           //Deserializing Object
+
+            Assert.AreEqual("VK", dataResponse.name);                                                   
             Assert.AreEqual(80000, dataResponse.salary);
 
         }
 
-        
+
         [TestMethod]
         public void OnAddingMultipleNewDatatoEmployeeList()
         {
+            //Arrange
             List<Employee> employeeList = new List<Employee>();
 
-            employeeList.Add(new Employee { name = "VK", salary = 80000 });
+            employeeList.Add(new Employee { name = "VK", salary = 80000 });                         //For Multiple Adding Data to EmployeeList
             employeeList.Add(new Employee { name = "MSD", salary = 100000 });
 
-            foreach(var item in employeeList)
+            foreach (var item in employeeList)
             {
-                RestRequest request = new RestRequest("/employees", Method.POST);
+                //Act
+                RestRequest request = new RestRequest("/employees", Method.POST);                  //For Adding Data to EmployeeList
                 JObject jObject = new JObject();
                 jObject.Add("name", item.name);
                 jObject.Add("salary", item.salary);
 
-                request.AddParameter("application/json", jObject, ParameterType.RequestBody);
+                request.AddParameter("application/json", jObject, ParameterType.RequestBody);          //Validation of RestRequest
 
 
-                IRestResponse response = client.Execute(request);
+                IRestResponse response = client.Execute(request);                                   //Executing the response
 
-                Assert.AreEqual(response.StatusCode, HttpStatusCode.Created);
-                Employee dataResponse = JsonConvert.DeserializeObject<Employee>(response.Content);
+
+                //Assert
+                Assert.AreEqual(response.StatusCode, HttpStatusCode.Created);                               //Checking Validation
+                Employee dataResponse = JsonConvert.DeserializeObject<Employee>(response.Content);          //Deserializing Object
 
                 Assert.AreEqual(item.name, dataResponse.name);
                 Assert.AreEqual(item.salary, dataResponse.salary);
             }
-            */
+        }
+            
 
 
         [TestMethod]
+
         public void UpdatingDatainEmployeeList()
         {
-
+            //Arrange
             RestRequest request = new RestRequest("employees/13", Method.PUT);
             JObject jobject = new JObject();
          
@@ -110,14 +125,30 @@ namespace RestSharpTesting
             
             request.AddParameter("application/json", jobject, ParameterType.RequestBody);
             
+            //Act
             IRestResponse response = client.Execute(request);
             
+
+            //Assert
             Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
             
             Employee dataResponse = JsonConvert.DeserializeObject<Employee>(response.Content);
 
             Assert.AreEqual(dataResponse.name, "Virat");
             Assert.AreEqual(dataResponse.salary, 150000);
+        }
+
+
+        [TestMethod]
+
+        public void DeleteDatainEmployeeList()
+        {
+            //Arrange
+            RestRequest request = new RestRequest("employees/13", Method.DELETE);
+            //Act
+            IRestResponse response = client.Execute(request);
+            //Assert
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
         }
     }
 }
